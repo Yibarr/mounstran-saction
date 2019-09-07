38dbff1925b67ad48c8d276ab414291a19aa19fd
Yael Ibarra <yibarr@yibarr.lenovo>
2019-09-07 14:42:46 -0500

    unbox not working

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,6 +1,6 @@
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import SimpleStorage from "../contracts/SimpleStorage.json";
+import ComplexStorage from "../contracts/ComplexStorage.json";
+import TutorialToken from "../contracts/TutorialToken.json";
 
 const options = {
   web3: {
